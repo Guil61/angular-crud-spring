050ff64ece62d300ebf80deddf32dc3ae2b9d2e2
Guil61 <andreguil061@gmail.com>
2024-04-29 22:55:15 -0300

    fix error

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 
+import {MatTableModule} from '@angular/material/table';
+
+
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit{
 
   courses: Course[] = [];
+displayedColumns: any;
 
   constructor(){
     //this.courses = [];
